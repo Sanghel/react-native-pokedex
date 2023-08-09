b0ba1985900f85f59192e80e77f1cfc6982c15af
Sanghel González <sanghel.gonzalez@walibre.com>
2023-08-09 14:01:19 -0400

    Pokedex Section and pokemon details

---
+++ src/screens/Pokedex.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text } from 'react-native'
+import { getPokemonApi, getPokemonDetailsByUrlApi } from '../api/pokemon'
+import PokemonList from '../components/PokemonList'
+
+export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([])
+  const [nextUrl, setNextUrl] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, [])
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonApi(nextUrl)
+      setNextUrl(response.next)
+      const pokemonsArray = []
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url)
+
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          image: pokemonDetails.sprites.other['official-artwork'].front_default
+        })
+      }
+      setPokemons([...pokemons, ...pokemonsArray])
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return (
+    <View>
+      <PokemonList pokemons={pokemons} loadPokemons={loadPokemons} isNext={nextUrl} />
+    </View>
+  )
+}
